687cd33279ffedcc3fd2efdd78c95667e353f7d7
kenshi1202 <kenshi1202@gmail.com>
2021-12-21 21:42:26 +0900

    Exercise 1

---
+++ vue-lab/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h3>テキストバインディング</h3>
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      msg: "Hello Vue.js!"
+    };
+  }
+});
+</script>
+
+<style scoped>
+.home {
+  text-align: left;
+}
+</style>
