288c87aa2aac3d67e6be1f01558c20ab22451298
kenshi1202 <kenshi1202@gmail.com>
2021-12-21 21:50:44 +0900

    Exercise 2

--- vue-lab/src/views/Home.vue
+++ vue-lab/src/views/Home.vue
@@ -2,16 +2,37 @@
   <div class="home">
     <h3>テキストバインディング</h3>
     <span>{{ msg }}</span>
+    <h3>リストレンダリング</h3>    
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
+declare interface Item {
+  id:number;
+  name:string;
+}
+
 export default Vue.extend({
   data() {
     return {
-      msg: "Hello Vue.js!"
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "keyboard"
+        },
+        {
+          id: 2,
+          name: "mouse"
+        }
+      ] as Item[]
     };
   }
 });
